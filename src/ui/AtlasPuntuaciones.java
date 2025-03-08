50cf6a42c80147d43e4901b0f432c69bc9931051
Alejandro <alejo.acosta.q@gmail.com>
2025-03-07 21:19:51 -0500

    Usted no nos enseño esto

--- src/ui/AtlasPuntuaciones.java
+++ src/ui/AtlasPuntuaciones.java
@@ -1,8 +1,22 @@
-// Análisis (completar)
+
 // Descripción del programa:
-// Entradas:
-// Salidas:
-// Ejemplo:
+/* 
+Entradas:
+1. Cantidad de rondas (valor maximo 5)
+2. Cantidad de jugadores (Maximo 10)
+3. Puntaje de cada jugador por cada ronda
+
+Salidas
+1. Por cada jugador su promedio y suma de puntaje en todas las rondas
+2. Jugador con puntaje mas alto, en caso de empate cualquiera de los tengan el puntaje maximo
+
+Proceso.
+1. Solicitar entradas  1 (rondas) y 2 (numero de jugadores)
+2. Crear los arreglos para almacenar la informacion
+3. Ciclo para solicitar los puntajes por jugador en cada ronda
+4. En un metodo hacer los calculos de promedio y suma total por jugador y almacenar en otro arreglo
+5. Imprimir los valores de puntaje para cada jugador y el mas alto
+*/
 
 
 package ui; // No olvidar especificar el paquete / carpeta de nuestro programa
@@ -21,24 +35,81 @@ public class AtlasPuntuaciones {
 		escaner = new Scanner(System.in);
 	}
 	
-	
+	/**
+	 * 
+	 */
 	public void run()
 	{
 		// Declaracion de todas las variables a usar en el programa
+		int rondas, jugadores, maximo = 0, ganador = 0;
+		double promedios;
+		System.out.println("Hola bienvenido a Atlas Puntajes, para iniciar el proceso necesitare la cantidad de rondas que haras (Maximo 10)");
+		rondas = leerEnteroMaximo(10, "rondas");
+		System.out.println("Bien Ahora dime la cantidad de judadores que participaran (Maximo 5)");
+		jugadores = leerEnteroMaximo(5, "jugadores");
 
+		int [][] partida = new int[jugadores][rondas];
 
-		// Notificacion al usuario de la solicitud de dato
+		for(int i = 0; i < jugadores ; i++) {
+			for(int j = 0; j< rondas ; j++){
+				System.err.println("digame los puntajes del jugador "+ (i+1) + " en la ronda " + (j+1) );
+				partida[i][j] = leerpuntaje();
+			}
+        }
 
-		// Capturamos el dato con nuestro Scanner (que se llama, escaner)
+		int totales[] = calculartotales(partida);
+		for(int i = 0; i < totales.length; i++){
+			System.out.println("jugador "+ (i+1)+" Puntaje total: "+ totales[i]+", Promedio: "+ (totales[i]/rondas));
+		}
+		
+		for (int i = 0; i < totales.length; i++) {
+			if(totales[i]>maximo){
+				maximo = totales[i];
+				ganador = i;
+			}
+		}
+		System.out.println("El jugador con mayor puntaje es, el judador "+ (ganador+1));
+	}
 
+	/**
+	 * Calcula los totales por cada jugador en la partida
+	 */
+	public int[] calculartotales(int[][] partida){
+		int[] totales = new int [partida.length];
+		for(int i = 0; i < partida.length ; i++) {
+			for(int j = 0; j< partida[0].length ; j++){
+				totales[i] = totales[i]+partida[i][j];
+				
+			}
+	 
+        }
+		return totales;
+	}
 
+	/**
+ 	* Lee un puntaje, valida que no supere el valor de 100
+ 	*/
+	public int leerpuntaje(){
+		int puntaje = escaner.nextInt();
+		while (puntaje > 100){
+			System.out.println("Valor invalido, el valor debe ser maximo 100");
+			puntaje = escaner.nextInt();
+		}
+		return puntaje;
+	}
 
-		// Realizamos los cálculos delegandolos a los métodos
-
-
-		// Notificación al usuario de los resultados
-
-
+	/**
+	 * Le un entero se asegura que el valor no supere maximo y si lo supera muestra el mensaje
+	 * maximo: valor maximo que el usuario puede digitar
+	 * mensaje: Mensaje que se le presentara al usuario cuando exceda el valor
+	 */
+	public int leerEnteroMaximo(int maximo, String mensaje){
+		int puntaje = escaner.nextInt();
+		while (puntaje > maximo){
+			System.out.println("Valor invalido, el valor maximo de "+ mensaje +" es de " + maximo);
+			puntaje = escaner.nextInt();
+		}
+		return puntaje;
 	}
 
 	public static void main(String[] args) {
@@ -46,34 +117,6 @@ public class AtlasPuntuaciones {
 		mainApp.run();
 	}
 
-	/**
-	 * Descripción: El método calcularSumaTotal permite ...
-	 * @param int[] numeros
-	 * @return 
-	 */
-    public int calcularSumaTotal(int[] numeros) {
-        // Completar
-    }
-
-	/**
-	 * Descripción: El método calcularPromedio permite ...
-	 * @param int[] numeros
-	 * @return 
-	 */
-    public double calcularPromedio(int[] numeros) {
-        // Completar
-    }
-
-	/**
-	 * Descripción: El método encontrarMayor permite ...
-	 * @param double[] numeros
-	 * @return 
-	 */
-    public double encontrarMayor(double[] numeros) {
-        // Completar
-    }
-
 	
-
 	
 }
